a0f3ce31dd07e3b2545a71fab8184ad50159e59d
Jens Balvig <jens@balvig.com>
2009-11-13 14:37:42 +0900

    Fixed Capistrano recipe

--- recipes/version_master.rb
+++ recipes/version_master.rb
@@ -2,18 +2,27 @@ namespace :deploy do
   
   desc "Bump version number and deploy"
   task :bump do
+    version.bump
     update_code
-    bump_version
     symlink
     restart
   end
   
-  desc "Bump version number"
-  task :bump_version do
-    version_file_path = 'config/version.yml'
-    rake_params = ENV['VERSION'] ? "VER=#{ENV['VER']}" : ''
-    system "rake version:bump #{rake_params}"
-    top.upload version_file_path, "#{release_path}/#{version_file_path}"
+  namespace :version do
+  
+    desc "Bump version number"
+    task :bump do
+      rake_params = ENV['VER'] ? "VER=#{ENV['VER']}" : ''
+      system "rake version:bump #{rake_params}"
+    end
+    
+    desc "Upload version config file"
+    task :upload_config do
+      VERSION_FILE_PATH = 'config/version.yml'
+      top.upload VERSION_FILE_PATH, "#{release_path}/#{VERSION_FILE_PATH}"
+    end
   end
+  
+  after 'deploy:update_code', 'deploy:version:upload_config'
   
 end
